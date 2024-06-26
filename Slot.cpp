664905e76a7a538c2cc293e3f059e4854470e4a3
Ran Shneor <102590409+Ranchook@users.noreply.github.com>
2024-06-26 17:20:09 +0300

    Update Slot.cpp
    Add comments

--- Slot.cpp
+++ Slot.cpp
@@ -1,7 +1,8 @@
 #include "Slot.h"
 
-int Slot::slotCounter = 1;
+int Slot::slotCounter = 1; // Initialize static member variable slotCounter to 1
 
+// Constructor: Initialize the Slot with a name and set its slot number
 Slot::Slot(const string name)
 {
 	setSlotName(name);
@@ -9,21 +10,24 @@ Slot::Slot(const string name)
 	slotCounter++;
 }
 
+// Destructor: Clean up Slot resources
 Slot::~Slot()
 {
-	slotName.clear();
+	slotName.clear(); // Clear the slot name string when the Slot object is destroyed
 }
 
+// Set Slot Number: Assign a number to the slot
 void Slot::setSlotNum(int num)
 {
 	slotNum = num;
 }
 
+// Set Slot Name: Assign a name to the slot
 void Slot::setSlotName(const string name)
 {
 	try 
 	{
-		if (name == "") BAD_INPUT; // if no name throw error
+		if (name == "") BAD_INPUT; // // If no name is provided, throw an error
 		this->slotName = name;
 	}
 	catch (const char* err)
@@ -32,8 +36,8 @@ void Slot::setSlotName(const string name)
 	}
 }
 
+// Print Slot: Print the slot
 const void Slot::printSlot()
 {
-	//print slot number
-	cout << "(slot " << getSlotNum() << ") ";
+	cout << "(slot " << getSlotNum() << ") "; // Print slot number in a specific format
 }
