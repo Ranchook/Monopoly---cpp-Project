428fbd5dec580abd018d1737417b7f65bdd91da3
Ran Shneor <102590409+Ranchook@users.noreply.github.com>
2024-06-26 09:01:19 +0300

    Update Instruction.h
    Add comments

--- Instruction.h
+++ Instruction.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Slot.h"
 
+// Define slotType enumeration
 typedef enum slotType{ Start, Ticket, Jail } slotType;
 
 class Instruction :public Slot
@@ -8,13 +9,12 @@ class Instruction :public Slot
 	slotType type;
 
 public:
-	Instruction(const string, slotType);
-	~Instruction() { cout << "Instruction Destructor\n";}; // using Slot destructor
-	Instruction(const string a):Slot(a) { cout << "can't copy Instruction\n";}; // copy constructor
-	Instruction operator = (string) { cout << "can't assign Instruction\n"; };
+	Instruction(const string, slotType);     // Constructor to initialize Instruction with name and slot type
+	~Instruction() { cout << "Instruction Destructor\n";}; // Destructor for Instruction
+	Instruction(const string a):Slot(a) { cout << "can't copy Instruction\n";};     // Copy constructor - not used in this case
+	Instruction operator = (string) { cout << "can't assign Instruction\n"; }; // Assignment (=) operator - not used in this case
 
-	const slotType getSlotType();
-	void setSlotType(const slotType&);
-	virtual const void printSlot();
-
+	const slotType getSlotType();     // Getter for slot type
+	void setSlotType(const slotType&);     // Setter for slot type
+	virtual const void printSlot();     // Method to print slot details
 };
