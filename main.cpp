369b01382f249969859ada692db02eb9ca5a1f43
Ran Shneor <102590409+Ranchook@users.noreply.github.com>
2024-06-26 17:25:12 +0300

    Update main.cpp
    Add comments

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-	int playersNum;
+	int playersNum; // Variable to store the number of players
 	cout << "Welcome to Monopoly!\n";
 	cout << "Please enter number of players: ";
 	try {
-		cin >> playersNum;
+		cin >> playersNum; // Input the number of players
 		if (cin.fail())
 			BAD_INPUT;
-		if (playersNum < 2)
+		if (playersNum < 2) // Check if the number of players is less than 2
 			throw "Not Enough Players! exiting...\n";
 	
-		GameEngine *temp = new GameEngine(playersNum);
-		temp->~GameEngine();
+		GameEngine *temp = new GameEngine(playersNum);  // Create a new GameEngine instance with the specified number of players
+		temp->~GameEngine(); // Manually call the destructor to clean up
 	}
 	catch (const char* err)
 	{
-		cout << err;
+		cout << err; // Catch and display any errors
 	}
 	system("pause");
 }
