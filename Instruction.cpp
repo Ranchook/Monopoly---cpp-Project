fb63fddff68041a5882d035edbf0ffbec31cdc20
Ran Shneor <102590409+Ranchook@users.noreply.github.com>
2024-06-26 08:59:18 +0300

    Update Instruction.cpp
    Add comments

--- Instruction.cpp
+++ Instruction.cpp
@@ -1,25 +1,31 @@
 #include "Instruction.h"
 
+// Constructor to initialize an Instruction object
 Instruction::Instruction(const string name, slotType t):Slot(name)
 {
 	setSlotType(t);
 }
 
+// Getter method for the slot type
 const slotType Instruction::getSlotType()
 {
 	return type;
 }
 
+// Setter method for the slot type
 void Instruction::setSlotType(const slotType &t)
 {
 	//STJ = start, ticket, jail
 	type = t;
 }
 
+// Method to print the slot details
 const void Instruction::printSlot()
 {
-	//print slot using the Slot method and add specific features
+    	// Call the base class method to print the slot
 	Slot::printSlot();
+	
+	// Print specific details for Instruction slot
 	cout << getSlotName() << endl;
 	cout << "=================\n" << endl;
 }
